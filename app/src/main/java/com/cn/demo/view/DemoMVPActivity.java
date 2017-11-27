5c2e20222ae1ac700edd72bbe4ca86d5b0d2128f
296409805@qq.com <liuyu920916>
2017-11-27 20:47:09 +0800

    MVP修改启动的activity

--- app/src/main/java/com/cn/demo/view/DemoMVPActivity.java
+++ app/src/main/java/com/cn/demo/view/DemoMVPActivity.java
@@ -1,11 +1,23 @@
 package com.cn.demo.view;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+import demo.com.cn.mvpdemoapp.R;
+
 /**
  * 视图层
  * Created by liuyu on 2017/11/27.
  */
 
-public class DemoMVPActivity implements IDemoMVPActView,DemoMVPActViewListener{
+public class DemoMVPActivity extends Activity implements IDemoMVPActView, DemoMVPActViewListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
 
 }
